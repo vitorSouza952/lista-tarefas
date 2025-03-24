6da7189f16bf0babdfe4ee74a85bcd41d0f261e0
Vitor Souza <vitorsouzagr80@gmail.com>
2025-03-24 20:15:44 -0300

    Terceiro commit.
    - Commit da versão 3.0.
    - Código formatado para melhor compreensão.

--- js/controle.js
+++ js/controle.js
@@ -1,86 +1,94 @@
-$(document).ready(function() {
-    function alternarMsgErro(msg) {
-        if (msg) {
-            $("#msg-erro").text(msg);
-            $("#msg-erro").addClass("ativo");
-        } else {
-            $("#msg-erro").removeClass("ativo");
-            $("#msg-erro").text("");
-        }
+$(document).ready(function () {
+  function alternarMsgErro(msg) {
+    if (!msg) {
+      $("#msg-erro").removeClass("ativo");
+    } else {
+      $("#msg-erro").addClass("ativo");
     }
 
-    function criarBotao(texto, classes) {
-        const botao = $("<button>", {
-            text: texto,
-            class: classes
-        });
+    $("#msg-erro").text(msg);
+  }
 
-        return botao;
-    }
+  function limparCampo() {
+    $("#texto-tarefa").val("");
+    if (window.innerWidth >= 992) $("#texto-tarefa").trigger("focus");
+    else $("#texto-tarefa").trigger("blur");
+  }
 
-    function adicionarTarefa(valTextoTarefa) {
-        const itemTarefa = $("<li>", {
-            class: "item-tarefa"
-        });
-
-        const textoTarefa = $("<p>", {
-            text: valTextoTarefa,
-            class: "texto-tarefa"
-        });
-
-        const areaAcoesTarefa = $("<div>", {
-            class: "area-acoes-tarefa"
-        });
-
-        const botaoAlternarEstado = criarBotao("Concluir", "botao botao-alternar-estado");
-        const botaoDeletar = criarBotao("Deletar", "botao botao-deletar");
-
-        itemTarefa.append([textoTarefa, areaAcoesTarefa]);
-        areaAcoesTarefa.append([botaoAlternarEstado, botaoDeletar]);
-        $("#lista-tarefas").append(itemTarefa);
-
-        if (!$("#conteudo-principal").hasClass("ativo")) $("#conteudo-principal").addClass("ativo");
-    }
-
-    function limparCampo() {
-        $("#texto-tarefa").val("");
-        if (window.innerWidth >= 992) $("#texto-tarefa").trigger("focus");
-        else $("#texto-tarefa").trigger("blur");
-    }
-
-    function alternarEstadoTarefa(botaoAlternarEstado) {
-        const itemTarefa = botaoAlternarEstado.closest(".item-tarefa");
-
-        itemTarefa.toggleClass("concluida");
-
-        botaoAlternarEstado.text(itemTarefa.hasClass("concluida") ? "Tornar Pendente" : "Concluir");
-    }
-
-    function deletarTarefa(botaoDeletar) {
-        botaoDeletar.closest(".item-tarefa").remove();
-        
-        if ($("#lista-tarefas").children().length === 0) $("#conteudo-principal").removeClass("ativo");
-    }
-
-    $("#form").on("submit", function(e) {
-        e.preventDefault();
-
-        const valTextoTarefa = $("#texto-tarefa").val();
-
-        if (!valTextoTarefa) {
-            alternarMsgErro("Erro: Digite uma tarefa!");
-        } else {
-            adicionarTarefa(valTextoTarefa);
-            alternarMsgErro(null);
-        }
-
-        limparCampo();
+  function criarBotao(classes, texto) {
+    const botao = $("<button>", {
+      class: classes,
+      text: texto,
     });
 
-    $("#lista-tarefas").on("click", function(e) {
-        const elAlvo = $(e.target);
+    return botao;
+  }
 
-        if (elAlvo.hasClass("botao-alternar-estado")) alternarEstadoTarefa(elAlvo);
-        if (elAlvo.hasClass("botao-deletar")) deletarTarefa(elAlvo);
+  function adicionarTarefa(valTextoTarefa) {
+    const itemTarefa = $("<li>", {
+      class: "item-tarefa",
     });
+
+    const textoTarefa = $("<p>", {
+      class: "texto-tarefa",
+      text: valTextoTarefa,
+    });
+
+    const areaAcoesTarefa = $("<div>", {
+      class: "area-acoes-tarefa",
+    });
+
+    const botaoAlternarEstado = criarBotao(
+      "botao botao-alternar-estado",
+      "Concluir"
+    );
+
+    const botaoDeletar = criarBotao("botao botao-deletar", "Deletar");
+
+    itemTarefa.append([textoTarefa, areaAcoesTarefa]);
+    areaAcoesTarefa.append([botaoAlternarEstado, botaoDeletar]);
+    $("#lista-tarefas").append(itemTarefa);
+
+    if (!$("#conteudo-principal").hasClass("ativo"))
+      $("#conteudo-principal").addClass("ativo");
+  }
+
+  function alternarEstadoTarefa(botaoAlternarEstado) {
+    const itemTarefa = botaoAlternarEstado.closest(".item-tarefa");
+
+    itemTarefa.toggleClass("concluida");
+
+    botaoAlternarEstado.text(
+      itemTarefa.hasClass("concluida") ? "Tornar Pendente" : "Concluir"
+    );
+  }
+
+  function deletarTarefa(botaoDeletar) {
+    botaoDeletar.closest(".item-tarefa").remove();
+
+    if ($("#lista-tarefas").children().length === 0)
+      $("#conteudo-principal").removeClass("ativo");
+  }
+
+  $("#form").on("submit", function (e) {
+    e.preventDefault();
+
+    const valTextoTarefa = $("#texto-tarefa").val();
+
+    if (!valTextoTarefa) {
+      alternarMsgErro("Erro: Por favor, digite uma tarefa.");
+    } else {
+      alternarMsgErro("");
+      adicionarTarefa(valTextoTarefa);
+    }
+
+    limparCampo();
+  });
+
+  $("#lista-tarefas").on("click", function (e) {
+    const elAlvo = $(e.target);
+
+    if (elAlvo.hasClass("botao-alternar-estado")) alternarEstadoTarefa(elAlvo);
+    if (elAlvo.hasClass("botao-deletar")) deletarTarefa(elAlvo);
+  });
 });
